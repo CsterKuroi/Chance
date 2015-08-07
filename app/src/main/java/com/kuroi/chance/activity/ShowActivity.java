8b77e946430437fcd092c7706f0c855bc7e656c9
Kuroi <412681778@qq.com>
2015-08-07 22:30:17 +0800

    first

---
+++ app/src/main/java/com/kuroi/chance/activity/ShowActivity.java
@@ -0,0 +1,35 @@
+package com.kuroi.chance.activity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.kuroi.chance.R;
+
+import java.io.File;
+
+public class ShowActivity extends ActionBarActivity {
+    private static final String ACTIVITY_TAG="LogDemo";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+        Log.d(ACTIVITY_TAG,"show");
+        Intent intent = getIntent();
+        String picName= intent.getStringExtra("picName");
+        Log.d(ACTIVITY_TAG,picName);
+        if(new File(picName).isFile()){
+            BitmapFactory.Options option = new BitmapFactory.Options();
+            option.inSampleSize = 4;
+            Bitmap bm = BitmapFactory.decodeFile(picName,option);
+            ImageView pic = (ImageView) findViewById(R.id.show_pic);
+            Log.d(ACTIVITY_TAG, "ok");
+            pic.setImageBitmap(bm);}
+    }
+
+
+}
